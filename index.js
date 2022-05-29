50cbf6ed6cfd6c84f0c6e2189d18960e252fe75a
Amit DebNath <amitdebnath@Amits-MacBook-Pro.local>
2022-05-29 23:00:13 +0600

    setup mongodb

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 3005 ;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_PASS}@cluster0.msmpdhl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect()
+        console.log('db connected');
+    }finally{
+
+    }
+}
+
+run().catch(console.dir)
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+  })
