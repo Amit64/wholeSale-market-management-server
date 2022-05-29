2704954f90a784c9e7f0b50f0df75f06917af931
Amit DebNath <amitdebnath@Amits-MacBook-Pro.local>
2022-05-29 23:51:26 +0600

    setup productb

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
-const express = require('express')
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const app = express()
-const port = process.env.PORT || 3005 ;
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3005;
 
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_PASS}@cluster0.msmpdhl.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-        await client.connect()
-        console.log('db connected');
-    }finally{
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("powertools-db").collection("products");
 
-    }
+    //get feature product
+    app.get("/limit-product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.limit(6).toArray();
+      res.send(products);
+    });
+  } finally {
+      //
+  }
 }
 
-run().catch(console.dir)
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-  })
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
